d16777c4255b739c072e05f319d8f4487d56482a
goncalveshigino <goncalveshigino20@gmail.com>
2021-07-02 21:49:15 +0100

    ytrt

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -1,57 +1,60 @@
 import 'dart:io';
 
 import 'package:path_provider/path_provider.dart';
+import 'package:qrread/src/models/scan-model.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
-
-
 class DBProvider {
-
-  
   static Database _database;
-  static final DBProvider db =  DBProvider._();
+  static final DBProvider db = DBProvider._();
 
-  DBProvider._(); 
+  DBProvider._();
 
-
-  Future<Database>get database async {
-
-    if ( _database != null) return _database;
+  Future<Database> get database async {
+    if (_database != null) return _database;
 
     _database = await initDB();
 
-
     return _database;
-
   }
-
 
   initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
 
+    final path = join(documentsDirectory.path, 'ScansDB.db');
 
-     Directory documentsDirectory = await getApplicationDocumentsDirectory();
-
-     final path = join( documentsDirectory.path, 'ScansDB.db');
-
-     return await openDatabase(
-
-       path, 
-       version: 1,
-       onOpen: (db) {},
-       onCreate: ( Database db, int version) async {
-          await db.execute(
-            'CREATE TABLE Scans ('
-               
-               'id INTEGER PRIMARY KEY,'
-               'tipo TEXT,'
-               'valor TEXT,'
-
-            ')'
-          );
-       }
-     );
-
+    return await openDatabase(path, version: 1, onOpen: (db) {},
+        onCreate: (Database db, int version) async {
+      await db.execute('CREATE TABLE Scans ('
+          'id INTEGER PRIMARY KEY,'
+          'tipo TEXT,'
+          'valor TEXT,'
+          ')');
+    });
   }
+
+  //Criar registros
+  novoScanRaw(ScanModel novoScan) async {
+    final db = await database;
+
+    final resp = await db.rawInsert("INSERT INTO Scans (id, tipo, valor) "
+        "VALUES ( '${ novoScan.id }','${ novoScan.tipo }', '${ novoScan.valor }' )"
+    );
+
+    return resp;
+  }
+
+     //Inserir valores na Base dados
+
+    novoScan( ScanModel novoScan ) async {
+
+       final db = await database;
+       final resp = db.insert('Scans', novoScan.toJson() ); 
+
+       return resp;
+    }
+
+    // SELECT - Obter iformacoes da base de dados
 
 }
