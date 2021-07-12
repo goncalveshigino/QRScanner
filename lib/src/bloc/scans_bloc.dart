1b321e8bced90e1af4b2ecab51723e5ade09d946
goncalveshigino <goncalveshigino20@gmail.com>
2021-07-12 10:21:05 +0100

    feito scan_bloc

---
+++ lib/src/bloc/scans_bloc.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+
+import 'package:qrread/src/providers/db_provider.dart';
+
+class ScansBloc {
+  static final ScansBloc _singleton = new ScansBloc._internal();
+
+  factory ScansBloc() {
+    return _singleton;
+  }
+
+  ScansBloc._internal() {
+    //Obter os Scans na base de dados
+    obterScans();
+  }
+
+  final _scanController = StreamController<List<ScanModel>>.broadcast();
+
+  Stream<List<ScanModel>> get scansStream => _scanController.stream;
+
+  dispose() {
+    _scanController?.close();
+  } 
+
+  //Obter todas as informacoes dos scans
+  obterScans() async {
+    _scanController.sink.add(await DBProvider.db.getTodosScans());
+  }
+
+  //Criar novo scan
+  adicionarSCan( ScanModel scan ) async {
+    
+    await DBProvider.db.novoScan(scan); 
+    obterScans();
+  }
+
+  //Eliminar Scan
+  eliminarScan( int id) async { 
+   
+   await DBProvider.db.deleteScan(id); 
+   obterScans();
+
+  }
+
+  //Eliminar todos os scans 
+   eliminarTodosScans() async {
+
+     await DBProvider.db.deleteAll(); 
+     obterScans();
+   }
+}
